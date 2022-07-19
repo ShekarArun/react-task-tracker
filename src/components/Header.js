6eb3e6a0642e1eefafcd826254e3e6b2ade4f87e
ShekarArun <shekar.arun@gmail.com>
2022-07-19 15:29:47 +0530

    feat: Add styling and button component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,19 @@
-const Header = () => {
+import PropTypes from 'prop-types'
+import Button from './Button'
+
+const Header = ({ title }) => {
+  const onClick = () => { console.log('Clicked!') }
+
   return (
-    <header>
-      <h1>Task tracker</h1>
+    <header className='header'>
+      <h1>{title}</h1>
+      <Button color='green' text='Add' onClick={onClick} />
     </header>
   )
 }
+
+Header.defaultProps = { title: 'Task tracker' }
+
+Header.propTypes = { title: PropTypes.string.isRequired }
 
 export default Header
