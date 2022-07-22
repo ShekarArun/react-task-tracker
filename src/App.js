067215e89c7144e4b124b27356703e83694d1be2
ShekarArun <shekar.arun@gmail.com>
2022-07-22 13:22:12 +0530

    feat: Add delete task functionality (UI only)

--- src/App.js
+++ src/App.js
@@ -4,17 +4,27 @@ import Header from './components/Header'
 import Tasks from './components/Tasks'
 
 function App () {
-  const [tasks] = useState([
-    { id: 1, msg: 'Item1' },
-    { id: 2, msg: 'Item2' },
-    { id: 3, msg: 'Item3' },
-    { id: 4, msg: 'Item4' }
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: 'Doctors Appointment',
+      day: 'Feb 5th at 2:30pm',
+      reminder: true
+    },
+    {
+      id: 2,
+      text: 'Meeting at School',
+      day: 'Feb 6th at 1:30pm',
+      reminder: true
+    }
   ])
+
+  const deleteTask = (id) => { setTasks(tasks.filter((task) => task.id !== id)) }
 
   return (
     <div className='container'>
       <Header />
-      <Tasks tasks={tasks} />
+      {tasks.length !== 0 ? <Tasks tasks={tasks} onDelete={deleteTask} /> : 'No tasks to show'}
     </div>
   )
 }
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,8 +1,9 @@
+import Task from './Task'
 
-const Tasks = ({ tasks }) => {
+const Tasks = ({ tasks, onDelete }) => {
   return (
     <>
-      {tasks.map((task) => (<h3 key={task.id}>{task.msg}</h3>))}
+      {tasks.map((task) => (<Task key={task.id} task={task} onDelete={() => onDelete(task.id)} />))}
     </>
   )
 }
